66d1f9004bddcb50bb589edad6d80d66e696cd1c
Flashrex <Dominik.Duda96@gmx.de>
2022-06-19 03:40:24 +0200

    Added: CookieRequest; Cleanup

---
+++ scripts/cookies.js
@@ -0,0 +1,142 @@
+var darkmode = true;
+
+checkCookies();
+function checkCookies() {
+    var cookiesEnabled = getCookie('cookieEnabled');
+
+    switch(cookiesEnabled) {
+        case "":
+            //first time visit
+            createCookieDiv(); 
+            break;
+
+        case "true":
+            darkmode = getCookie('darkmode') === "" ? true : getCookie('darkmode') === "true";
+            applyDarkmode();
+            break;
+
+        case "false":
+            applyDarkmode();
+            break;
+    }
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+  
+function createCookieDiv() {
+    const div = document.createElement("div");
+    div.classList.add('cookieDiv');
+    div.classList.add('bg-dark-contrast');
+    
+  
+    const headline = document.createElement("h3");
+    headline.innerHTML = "Wir lieben Cookies!";
+    headline.classList.add('font-darkmode-white');
+    headline.classList.add('headline-text');
+    headline.style.marginTop = "4rem";
+  
+    const text = document.createElement("p");
+    text.innerHTML = "Auf dieser Website werden Cookies zur Verbesserung des Nutzungserlebnisses verwendet. <br>Die Cookies dienen lediglich zur Speicherung von visuellen Präferenzen. Es werden keine nutzerbezogenen Daten gespeichert!";
+    text.classList.add('font-darkmode-white');
+    text.style.width = "80%";
+    text.style.marginLeft = "10%";
+  
+    const buttonAccept = document.createElement("input");
+    buttonAccept.classList.add('button');
+    buttonAccept.value = "Akzeptieren";
+    buttonAccept.onclick = onUserAcceptedCookies;
+    buttonAccept.style.textAlign = "center";
+  
+    const buttonDecline = document.createElement("input");
+    buttonDecline.classList.add('button');
+    buttonDecline.value = "Ablehnen";
+    buttonDecline.onclick = onUserDeclinedCookies;
+    buttonDecline.style.textAlign = "center";
+  
+    const buttonContainer = document.createElement("div");
+    buttonContainer.appendChild(buttonAccept);
+    buttonContainer.appendChild(buttonDecline);
+  
+    div.appendChild(headline);
+    div.appendChild(text);
+    div.appendChild(buttonContainer);
+  
+    document.body.append(div);
+}
+  
+function removeCookieDiv() {
+    const cookieDiv = document.querySelector('.cookieDiv');
+    cookieDiv.remove();
+}
+  
+function onUserAcceptedCookies() {
+    removeCookieDiv();
+  
+    setCookie("cookieEnabled", "true", 30);
+}
+  
+function onUserDeclinedCookies() {
+    removeCookieDiv();
+    setCookie("cookieEnabled", "false", 30);
+}
+
+/* Darkmode */
+function toggleDarkMode() {
+    darkmode = !darkmode;
+    //document.cookie = `darkmode=${darkmode};SameSite=None;secure`;
+    setCookie('darkmode', darkmode, 30);
+    applyDarkmode();
+}
+
+function applyDarkmode() {
+    //console.log("Cookie: " +getCookie('darkmode') +", darkmode: " +darkmode);
+
+    document.querySelectorAll(darkmode ? ".bg-light" : ".bg-dark").forEach(e => {
+        e.classList.add(darkmode ? "bg-dark" : "bg-light");
+        e.classList.remove(darkmode ? "bg-light" : "bg-dark");
+    })
+
+    document.querySelectorAll(darkmode ? ".bg-light-contrast" : ".bg-dark-contrast").forEach(e => {
+        e.classList.add(darkmode ? "bg-dark-contrast" : "bg-light-contrast");
+        e.classList.remove(darkmode ? "bg-light-contrast" : "bg-dark-contrast");
+    })
+
+    document.querySelectorAll(darkmode ? ".font-lightmode-black" : ".font-darkmode-white").forEach(e => {
+        e.classList.add(darkmode ? "font-darkmode-white" : "font-lightmode-black");
+        e.classList.remove(darkmode ? "font-lightmode-black" : "font-darkmode-white");
+    })
+
+    document.querySelectorAll(darkmode ? ".font-lightmode-grey" : ".font-darkmode-grey").forEach(e => {
+        e.classList.add(darkmode ? "font-darkmode-grey" : "font-lightmode-grey");
+        e.classList.remove(darkmode ? "font-lightmode-grey" : "font-darkmode-grey");
+    })
+
+    let icon = document.getElementById("dm-icon");
+    icon.classList.add(!darkmode ? "svg-color-dark" : "svg-color-light");
+    icon.classList.remove(!darkmode ? "svg-color-light" : "svg-color-dark");
+    icon.src = darkmode ? "images/sun.svg" : "images/moon.svg";
+}
+
+
+
