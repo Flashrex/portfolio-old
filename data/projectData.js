283f533be483e7edb6f605527c17f152ff543087
Flashrex <Dominik.Duda96@gmx.de>
2022-06-19 06:00:21 +0200

    Added: Tags, Search By Name/Tag;Removed: Languages

--- data/projectData.js
+++ data/projectData.js
@@ -2,7 +2,7 @@ var projects = [
     {
         headline: "Website",
         description: "Die Website, die du dir gerade anschaust.",
-        languages: "Html, Css, Js",
+        tags: ["html", "javascript", "css"],
         image_url: "images/website_logo.png",
         file: "",
         repository: "https://github.com/Flashrex/Website"
@@ -10,7 +10,7 @@ var projects = [
     {
         headline: "Bugtracker",
         description: "App um Bugs oder Tasks innerhalb eines Projekts zu tracken.",
-        languages: "Typescript, NodeJs",
+        tags: ["typescript", "nodejs", "express"],
         image_url: "images/bugtracker.png",
         file: "",
         repository: "https://github.com/Flashrex/BugTracker"
@@ -18,7 +18,7 @@ var projects = [
     {
         headline: "Steam Market Tracker",
         description: "Tracke Preise von CS:GO Items auf dem Steam Community Market Place.",
-        languages: "NodeJs, Javascript",
+        tags: ["nodejs", "javascript", "html/css", "express"],
         image_url: "images/steammarkettracker.png",
         file: "",
         repository: "https://github.com/Flashrex/"
@@ -26,7 +26,7 @@ var projects = [
     {
         headline: "Conway's Game of Life",
         description: "Der Klassiker Game of Life von John Horton Conway.",
-        languages: "Javascript",
+        tags: ["javascript"],
         image_url: "images/gameoflife.png",
         file: "projects/games/gameoflife/index.html",
         repository: "https://github.com/Flashrex/flashrex.github.io/tree/master/projects/games/gameoflife"
@@ -34,7 +34,7 @@ var projects = [
     {
         headline: "Pong!",
         description: "Der Urvater der modernen Videospiele aus dem Jahre 1972.<br>Enthält 2-Spieler Modus.",
-        languages: "Javascript",
+        tags: ["nodejs", "javascript"],
         image_url: "images/pong.png",
         file: "projects/games/pong/index.html",
         repository: "https://github.com/Flashrex/flashrex.github.io/tree/master/projects/games/pong"
@@ -42,7 +42,7 @@ var projects = [
     {
         headline: "Sorting Algorithm Visualization",
         description: "Visualisierung von verschiedenen Sortier-Algorithmen.<br>(Derzeit nur Quick Sort)",
-        languages: "Javascript",
+        tags: ["nodejs", "javascript"],
         image_url: "images/sorting.png",
         file: "projects/sorting/index.html",
         repository: "https://github.com/Flashrex/flashrex.github.io/tree/master/projects/sorting"
@@ -50,7 +50,7 @@ var projects = [
     {
         headline: "VR-Suit",
         description: "Meine erste eigene kleine Website für ein Schulprojekt.",
-        languages: "Html, Css, Js",
+        tags: ["html/css", "javascript"],
         image_url: "projects/vr_suit/images/logo.png",
         file: "projects/vr_suit/index.html",
         repository: "https://github.com/Flashrex/flashrex.github.io/tree/master/projects/vr_suit"
--- scripts/projects.js
+++ scripts/projects.js
@@ -15,13 +15,18 @@ function addItemToDomTree(item) {
 
     let fontcolor = darkmode ? "font-darkmode-white" : "font-lightmode-black";
 
+    let tags = "";
+    for(var i = 0; i < item.tags.length; i++) {
+        tags += `<div class="tag">${item.tags[i]}</div>`;
+    }
+
     elem.innerHTML = `
         <img src="${item.image_url}" alt=img_${item.headline}>
         <div class="item-info ${fontcolor}">
             <h3>${item.headline}</h3>
             <p>${item.description}</p>
-            <p>Languages: ${item.languages}</p>
-            <div class="btn_container">
+            <div class="tag-container">${tags}</div>
+            <div class="btn-container">
                 ${item.file !== "" ? `<button class="button"><a href=${item.file}>Try Out</a></button>` : ``}
                 ${item.repository.includes("github") ? '<button class="button"><a href="' +item.repository +'" target="_blank">Show Code</a></button>' : '<button class="button"><a href="' +item.repository +'" target="_blank">Open on Gitlab</a></button>'}
             </div>
@@ -29,4 +34,20 @@ function addItemToDomTree(item) {
     `;
 
     document.getElementById("project_container").appendChild(elem);
+}
+
+
+/* Suche nach Tag */
+function onSearchInput() {
+    if(projects === undefined) return;
+
+    document.querySelector("#project_container").innerHTML = "";
+
+    let input = document.querySelector("#input_tag").value.toLowerCase();
+
+    for(var i = 0; i < projects.length; i++) {
+        if(projects[i].headline.toLowerCase().includes(input) || projects[i].tags.find(t => t.toLowerCase().includes(input))) {
+            addItemToDomTree(projects[i]);
+        }
+    }
 }
